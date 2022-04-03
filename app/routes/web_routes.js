50ea56d01caa8a384bffcf005a8cefee56b2c2ed
ornery-mouse <cory.sorel@gmail.com>
2022-04-03 11:37:22 -0500

    mongoose troubleshooting

--- app/routes/web_routes.js
+++ app/routes/web_routes.js
@@ -14,7 +14,7 @@ router.get('/search', (req, res) => {
     if (!req.query.countyName || !req.query.state) {
         res.render('search')
     } else {
-        County.findOne({ abbrev: req.query.state.toUpperCase(), name: { $in: [req.query.countyName]} })
+        County.findOne({ abbrev: req.query.state.toUpperCase(), name: { $regex: req.query.countyName} })
             .then(foundCounty => {
                 // const foundCounty = foundState.counties.filter(county => county.county.includes(req.query.countyName))
                 console.log(foundCounty)
